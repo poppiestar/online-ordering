016c29a6a469bc5897ea1aa0c06c957d1a6a1b21
Drew Miller <poppiestar@users.noreply.github.com>
2017-08-01 20:12:15 +0100

    Add basic hapi server

---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+
+import { Server } from 'hapi';
+import RootHandler from './routes/app';
+
+const server = new Server();
+
+server.connection({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: RootHandler
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    console.log(`Server running at: ${server.info.uri}`);
+});
---
+++ src/server/routes/app.js
@@ -0,0 +1,6 @@
+
+const rootHandler = (request, reply) => {
+    reply('Hi there');
+};
+
+export default rootHandler;
