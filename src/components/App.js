4ecd83fc14691cf02b8e5e37a8c7bb71ba77c96f
Drew Miller <poppiestar@users.noreply.github.com>
2017-08-02 23:11:32 +0100

    Add Menu to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 
 import React from 'react';
+
 import CuisineSelectorContainer from './CuisineSelectorContainer';
+import MenuContainer from './MenuContainer';
 
 const App = () =>
     <div>
         <CuisineSelectorContainer />
+        <MenuContainer />
     </div>;
     
 export default App;
