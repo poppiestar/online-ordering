2737787f209f452afc287198f809a31f09dd0ed1
Drew Miller <poppiestar@users.noreply.github.com>
2017-08-02 20:07:01 +0100

    Add test for selecting a cuisine option

--- src/test/components/CuisineSelector-test.js
+++ src/test/components/CuisineSelector-test.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Lab from 'lab';
+import sinon from 'sinon';
 import { expect } from 'code';
 import { shallow } from 'enzyme';
 
@@ -39,6 +40,15 @@ suite('Cuisine Selector component', () => {
         const wrapper = shallow(<CuisineSelector options={props.options} />);
         
         expect(wrapper.find('select option').length).to.equal(6);
+        done();
+    });
+
+    test("should select the cuisine when an option is selected", (done) => {
+        const changeSpy = sinon.spy();
+        const wrapper = shallow(<CuisineSelector options={props.options} selectCuisine={changeSpy} />);
+
+        wrapper.find('select').simulate('change', {target: { value : 2 }});
+        expect(changeSpy.callCount).to.equal(1);
         done();
     });
 });
