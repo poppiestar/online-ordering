26042df30a9dc3e5e11ba855df23c3e2be7677c5
Drew Miller <poppiestar@users.noreply.github.com>
2017-08-02 19:28:16 +0100

    Add options to component

--- src/components/CuisineSelector.js
+++ src/components/CuisineSelector.js
@@ -9,7 +9,14 @@ type Props = {
 const CuisineSelector = ({ options }: Props) =>
     <div>
         <h1>What taste sensation are you after tonight?</h1>
-        <select></select>
+        <select>
+            <option>Select cuisine:</option>
+            {
+                options.map(option =>
+                    <option key={option.id} value={option.id}>{option.name}</option>
+                )
+            }
+        </select>
     </div>;
 
 export default CuisineSelector;
