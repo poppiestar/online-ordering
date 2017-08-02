8b46cc3fab5b4d9c23d046008301f6d66fdaec8d
Drew Miller <poppiestar@users.noreply.github.com>
2017-08-02 23:11:13 +0100

    Populate with better data

--- src/reducers/cuisines.js
+++ src/reducers/cuisines.js
@@ -3,10 +3,9 @@ const initialState = [
     { id: 1, name: "Chinese" },
     { id: 2, name: "Italian" },
     { id: 3, name: "Thai" },
-    { id: 4, name: "Turkish" },
-    { id: 5, name: "Japanese" },
-    { id: 6, name: "Burrito" },
-    { id: 7, name: "Pizza" }
+    { id: 4, name: "Japanese" },
+    { id: 5, name: "Burrito" },
+    { id: 6, name: "Pizza" }
 ];
 
 const cuisines = (state = initialState, action) => {
--- src/server/data/menus.js
+++ src/server/data/menus.js
@@ -1,13 +1,34 @@
 const menus = {
-    1: { stuff: 'things' },
-    2: { things: 'junk' },
-    3: { junk: 'wotsit' },
-    4: { wotsit: 'flotsam' },
-    5: { flotsam: 'jetsam' },
-    6: { jetsam: 'ramjet' },
-    7: { ramjet: 'roger' },
-    8: { roger: 'wilco' },
-    9: { wilco: 'amadeus' }
+    1: [
+        { id: 'C:LEM', name: "Lemon Chicken", price: 4.99 },
+        { id: 'C:SPS', name: "Salt and Pepper Squid", price: 6 },
+        { id: 'C:SFR', name: "Spicy Fried Rice", price: 2.50 }
+    ],
+    2: [
+        { id: 'I:CAR', name: "Fettucine Carbonara", price: 5.99 },
+        { id: 'I:PEN', name: "Penne Arrabiata", price: 4.99 },
+        { id: 'I:GEL', name: "Gelato", price: 3 }
+    ],
+    3: [
+        { id: 'T:SCR', name: "Spicy Crackers", price: 2.49 },
+        { id: 'T:YCR', name: "Yellow Curry", price: 6.99 },
+        { id: 'T:PAR', name: "Prawn Parcels", price: 3.99 }
+    ],
+    4: [
+        { id: 'J:SUS', name: "Sushi", price: 8.99 },
+        { id: 'J:MIS', name: "Miso Soup", price: 1.50 },
+        { id: 'J:EDM', name: "Edamame", price: 3.99 }
+    ],
+    5: [
+        { id: 'B:CHK', name: "Chicken Burrito", price: 6.99 },
+        { id: 'B:PWN', name: "Prawn Burrito", price: 7.99 },
+        { id: 'B:BEF', name: "Beef Burrito", price: 6.99 }
+    ],
+    6: [
+        { id: 'P:SIZ', name: "The Sizzler", price: 16.99 },
+        { id: 'P:HAW', name: "Hawaiian", price: 15.99 },
+        { id: 'P:SUP', name: "Supreme", price: 17.99 }
+    ]
 };
 
 export default menus;
